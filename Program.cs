2dbff01d0e4373b877920d05c3b20d1a76bc062c
Deshan John Kothalawalage <deshan.john@bbzbl-it.ch>
2026-01-30 08:25:43 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using BildungsBericht.Components;
+using BildungsBericht.Services;
+
+namespace BildungsBericht
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddSingleton<BenutzerService>();
+            builder.Services.AddSingleton<BerichteService>();
+            builder.Services.AddSingleton<SelbstbewertungService>();
+            builder.Services.AddHttpClient();
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+            app.MapControllers();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
